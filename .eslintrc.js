daa1c13e97eddb295be37e894f342164cf695b5d
紀良諭 <jeffchi@jiliangyude-MacBook-Pro.local>
2023-04-22 16:59:40 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  // 只作用于当前目录
+  root: true,
+  // 运行环境
+  env: {
+    node: true,
+    es6: true,
+  },
+  // 解析器
+  parser: '@babel/eslint-parser',
+  // 解析器选项
+  parserOptions: {
+    sourceType: 'module',
+  },
+  // 插件
+  plugins: ['import'],
+  // 扩展配置
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'plugin:import/recommended',
+    'prettier',
+  ],
+  // 启用规则
+  rules: {
+    "vue/multi-word-component-names": 0
+  },
+  // 全局变量
+  globals: {
+    h: true,
+  },
+  // 为指定文件指定处理器
+  overrides: [
+    {
+      files: ['*.vue', '*.jsx'],
+      parser: 'vue-eslint-parser',
+      parserOptions: {
+        ecmaVersion: 2018,
+      },
+      rules: {}
+    }
+  ],
+}

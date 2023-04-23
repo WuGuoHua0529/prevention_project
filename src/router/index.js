a12ddbd6f64bc3aba68e53a39d627b88f7cf4dcb
紀良諭 <jeffchi@jiliangyude-MacBook-Pro.local>
2023-04-23 17:57:40 +0800

    災害可能發生的地方-初版

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Earthquake from '../views/Earthquake.vue';
 import Protect from "../views/Protect.vue";
+import Disaster from '../views/Disaster.vue'
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/Earthquake', name: 'Earthquake', component: Earthquake },
-  { path: '/Protect', name: 'Protect', component: Protect }
+  { path: '/Protect', name: 'Protect', component: Protect },
+  { path: '/Disaster', name: 'Disaster', component: Disaster }
 ]
 
 const router = createRouter({
